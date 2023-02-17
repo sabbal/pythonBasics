4ea3089e07cea5191933e289fae33ab9bd7ab68f
sabbal <117453049+sabbal@users.noreply.github.com>
2023-02-17 23:34:31 +0530

    For Loop in py
    This code contains multiplying Numbers in Range

---
+++ For_loop.py
@@ -0,0 +1,7 @@
+#This Code for Loop mutplying the tabel
+
+number = int (input("Enter a Number : "))
+for count in range (1 , 11) :
+    prodrct = number * count
+    print(number , "x" , count, "=", prodrct)
+
