326c4cc892d91b285caa5a3c06755705c47adf37
sabbal <117453049+sabbal@users.noreply.github.com>
2023-02-17 15:41:44 +0530

    Converting one data type into another

---
+++ Data_types_Conversion.py
@@ -0,0 +1,12 @@
+# here a and c is given as string for later to convert it in int
+a = "89"
+b = 64
+c = "66"
+d = 36
+
+a = int(a)
+c = int(c)
+print("The Sum of a and b =" ,a + b)
+print("The Sum of c and d =" ,c + d)
+# here we cannot operate the String with integer
+# before operating we have to change the data type into int
