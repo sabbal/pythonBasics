64027e01cb3a75efd73f529cb1dcbf4c124de5b5
sabbal <117453049+sabbal@users.noreply.github.com>
2023-02-17 17:01:05 +0530

    Assignment Operators in py (+=, -=, *= , /= , **=)

---
+++ Assinment_Operaters.py
@@ -0,0 +1,34 @@
+a = 56
+b = 22
+c = 16
+d = 48
+# Addition Assignment of Two variabels (+=)
+a+=b
+print("Sum Of a += b = ",a)
+c+=d
+print("Sum of c += d = ",c)
+# Subtraction Assignment of Two variabels (-=)
+a-=b
+print("Subtraction Assignment Of a -=b = ",a)
+c-=d
+print("Subtraction Assignment Of c -= d = ",c)
+# Multiplication Assignment of Two variabels (*=)
+a*=b
+print("Multipel Of a *= b = ",)
+d*=c
+print("Multipel Of d *= c = ",d)
+# Division Assignment of two variabels (/=)
+a/=b
+print("Devision Of a and b = ", a)
+c/=d
+print("Devision Of c and d = ", c)
+#Remainder Assignment of two variabels (%=)
+c%=d
+print("Mode Of a %= b = ", a)
+c%=d
+print("Mode Of c %= d = ", c)
+# Exponent Assignment of two variabels (**=)
+a**=b
+print("Power Of a **= b = ",a)
+c**=d
+print("Power Of c **= d = ", c)
