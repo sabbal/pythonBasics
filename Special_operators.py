de854faf1418a690d9e37ed297df5a64aed3ae2e
sabbal <117453049+sabbal@users.noreply.github.com>
2023-02-17 19:33:49 +0530

    special Operators in py

---
+++ Special_operators.py
@@ -0,0 +1,16 @@
+a = 66
+b = 66
+c = "Sabeel"
+d = "Sabeel"
+# is and is not is identical operators
+# Shows true if they are identical else False
+print(a is b)
+print(c is d)
+print(a is not b)
+print(c is not d)
+# in and not in Comes in Membeship oerster
+# it helps to know if the value is present in variabel or not
+# True if Yes False value is not present
+print(3 in a)
+print("s" not in c)
+print("j" not in d)
