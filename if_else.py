0216bc605d2fb64f01ad8fdad57138d16cc61cea
sabbal <117453049+sabbal@users.noreply.github.com>
2023-02-17 22:27:58 +0530

    if Else Looping Statement

---
+++ if_else.py
@@ -0,0 +1,8 @@
+Marks = int (input("Enter Your Schore : "))
+if Marks > 100  or Marks <= 35 :
+    print("Invalid marks")
+elif Marks > 35 :
+    print("Pappu Pass Ho Gayaa")
+else :
+    print("Khatam Tata Good Bye Gayaa")
+    print("Padai likae karo Iays Bano Desh ko Sambhalo")
