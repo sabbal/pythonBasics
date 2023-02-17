e784a9c589ae1dbc753359baccce477f3498f2d3
sabbal <117453049+sabbal@users.noreply.github.com>
2023-02-17 17:27:59 +0530

    Comparison operators in Py

---
+++ Comaprision_operaters.py
@@ -0,0 +1,23 @@
+a = 96
+b = 45
+c = 56
+d = 78
+
+# equal to operator(==)
+print('a == b =', a == b)
+print('c == d =', c == d)
+# not equal to operator(!=)
+print('a != b =', a != b)
+print('c != d =', c != d)
+# greater than operator (>)
+print('a > b =', a > b)
+print('c > d =', c > d)
+# less than operator (<)
+print('a < b =', a < b)
+print('c < d =', c < d)
+# greater than or equal to operator (>=)
+print('a >= b =', a >= b)
+print('c >= d =', c >= d)
+# less than or equal to operator (<=)
+print('a <= b =', a <= b)
+print('c <= d =', c <= d)
