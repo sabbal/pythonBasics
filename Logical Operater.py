74f6aad63d4d1d31a96e6882f42d54f339e125d8
sabbal <117453049+sabbal@users.noreply.github.com>
2023-02-17 17:47:17 +0530

    Logical Operators in Py

---
+++ Logical Operater.py
@@ -0,0 +1,16 @@
+a = 88
+b = 35
+c = 26
+d = 18
+
+# Logical Operator And (Both of them are equal then true else False)
+print(a and b)
+print(c and d)
+# logical Operator or (Atlest one Of the Operators is true then True else False)
+print("Greater is",a or b)
+print("Greater is",c or d)
+# logical Operator not ( true if operand is False)
+print("Not of a =", not a)
+print("Not of b =", not b)
+print("Not of c =", not c)
+print("Not of d =", not d)
